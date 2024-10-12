b513c80e202eb62de575eabb530f0a2dd57ccf49
devesh1808 <devesh1808@gmail.com>
2024-10-12 13:57:36 +0530

    used object destructuring in CoreConcept component instead of props parameter

--- reactEssentialsProject/src/App.jsx
+++ reactEssentialsProject/src/App.jsx
@@ -23,12 +23,13 @@ function Header() {
 }
 
 // making components reusable using props
-function CoreConcept(props) {
+// using object destructuring instead of props parameter
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
