30acd43117398bdf9910f9aa49a8ea5b01c097ad
devesh1808 <devesh1808@gmail.com>
2024-10-16 21:07:25 +0530

    understood that using a regular variable in react we can't update the UI again and again as by default react component update only once

--- reactEssentialsProject/src/App.jsx
+++ reactEssentialsProject/src/App.jsx
@@ -6,10 +6,16 @@ import TabButton from "./components/TabButton.jsx";
 
 function App() {
 
+  let tabContent = "Please click a button";
+
   function handleSelect(selectedButton){
     // selectedButton => 'components', 'jsx', 'props', 'state'
-    alert(selectedButton);
+    //alert(selectedButton);
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
+
+  console.log("APP COMPONENT");
 
   return (
     <div>
@@ -35,7 +41,7 @@ function App() {
             <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {tabContent}
         </section>
       </main>
     </div>
