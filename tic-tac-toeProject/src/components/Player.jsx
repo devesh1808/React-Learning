6bda0b8c0a55a9a5b944528442ec6ec539596eda
devesh1808 <devesh1808@gmail.com>
2024-10-22 00:07:36 +0530

    added useState in Player component to do conditional rendering on the basis of edit button

--- tic-tac-toeProject/src/components/Player.jsx
+++ tic-tac-toeProject/src/components/Player.jsx
@@ -1,11 +1,22 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing(true);
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="input" required />
+        )}
+        {/* <span className="player-name">{name}</span> */}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
