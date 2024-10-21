7afc2cc7bb3011341a626ac2bd772f6f6660d5de
devesh1808 <devesh1808@gmail.com>
2024-10-21 13:53:04 +0530

    created 2 components inside components folder and added the App.jsx code to components to make App.jsx clean

---
+++ reactEssentialsProject/src/components/Examples.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { EXAMPLES } from "../data.js";
+import TabButton from "./TabButton.jsx";
+
+function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    //console.log(selectedTopic);
+  }
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        {/* Children Prop */}
+        {/* Passing onSelect function function as a prop to TabButton component */}
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+
+      {/* Conditional Rendering */}
+      {!selectedTopic ? (
+        <p>Please select a topic.</p>
+      ) : (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default Examples;
