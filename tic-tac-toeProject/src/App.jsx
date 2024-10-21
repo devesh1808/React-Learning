05ae88bac29e88c9ecf3ffe11adc52aea2576c38
devesh1808 <devesh1808@gmail.com>
2024-10-21 21:04:38 +0530

    added Edit button for both players

--- tic-tac-toeProject/src/App.jsx
+++ tic-tac-toeProject/src/App.jsx
@@ -4,17 +4,23 @@ function App() {
       <div id="game-container">
         <ol id="players">
           <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
           </li>
           <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
           </li>
         </ol>
         GAME BOARD
       </div>
-      LOG 
+      LOG
     </main>
   );
 }
